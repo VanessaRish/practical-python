36908466f9c9de43344606a18212bb4171d6f4e8
Vanessa Rish <shelterly@gmail.com>
2023-11-20 22:29:07 +0100

    1.6 files done

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,8 @@
-# pcost.py
-#
-# Exercise 1.27
+total_amount = 0
+with open("./Data/portfolio.csv", "r") as file:
+    lines = file.readlines()
+    for line in lines[1:]:
+        row = line.strip().split(",")
+        total_amount += int(row[1]) * float(row[2])
+
+print(f"Total cost {total_amount}")
