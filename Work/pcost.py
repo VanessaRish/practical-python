6e3afb0fcb891db4687e374eed2b2e4a0d25c872
Vanessa Rish <shelterly@gmail.com>
2023-11-22 19:53:33 +0100

    Done 1.7 functions

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,8 +1,25 @@
-total_amount = 0
-with open("./Data/portfolio.csv", "r") as file:
-    lines = file.readlines()
-    for line in lines[1:]:
-        row = line.strip().split(",")
-        total_amount += int(row[1]) * float(row[2])
+import sys
+import csv
 
-print(f"Total cost {total_amount}")
+
+def portfolio_cost(filename):
+    total_amount = 0
+    with open(filename, "r") as file:
+        lines = csv.reader(file)
+        _ = next(lines)
+        for line in lines:
+            try:
+                total_amount += int(line[1]) * float(line[2])
+            except ValueError as e:
+                print("Value is missing!")
+                print(e)
+                continue
+    return total_amount
+
+
+filename = "./Data/portfolio.csv"
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+
+cost = portfolio_cost(filename)
+print(f"Total cost: {cost}")
