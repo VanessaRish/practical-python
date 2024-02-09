d28ef72a9f1499ab5e0e375bd92e74bfe051755e
Vanessa Rish <shelterly@gmail.com>
2024-02-09 18:45:29 +0100

    6.4 More Generators

--- Work/ticker.py
+++ Work/ticker.py
@@ -4,43 +4,23 @@ import report
 import tableformat
 
 
-def convert_types(rows, types):
-    for row in rows:
-        yield [func(val) for func, val in zip(types, row)]
-
-
-def make_dicts(rows, headers):
-    for row in rows:
-        yield dict(zip(headers, row))
-
-
-def select_columns(rows, indices):
-    for row in rows:
-        yield [row[index] for index in indices]
-
-
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
-
-
 def parse_stock_data(lines):
     rows = csv.reader(lines)
-    rows = select_columns(rows, [0, 1, 4])
-    rows = convert_types(rows, [str, float, float])
-    rows = make_dicts(rows, ['name', 'price', 'change'])
+    rows = ([row[index] for index in [0, 1, 4]] for row in rows)
+    types = [str, float, float]
+    rows = ([func(val) for func, val in zip(types, row)] for row in rows)
+    rows = (dict(zip(["name", "price", "change"], row)) for row in rows)
     return rows
 
 
 def ticker(portfolio, logfile, fmt):
     portfolio = report.read_portfolio(portfolio)
     rows = parse_stock_data(follow(logfile))
-    rows = filter_symbols(rows, portfolio)
+    rows = (row for row in rows if row["name"] in portfolio)
     formatter = tableformat.create_formatter(fmt)
-    formatter.headings(['Name', 'Price', 'Change'])
+    formatter.headings(["Name", "Price", "Change"])
     for row in rows:
-        formatter.row([row['name'], f"{row['price']:0.2f}", f"{row['change']:0.2f}"])
+        formatter.row([row["name"], f"{row['price']:0.2f}", f"{row['change']:0.2f}"])
 
 
 if __name__ == "__main__":
